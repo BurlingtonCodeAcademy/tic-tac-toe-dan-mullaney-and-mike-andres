132f521e359d9cb042779e5fbaa22bacced7b76c
Daniel Mullaney <danftv@gmail.com>
2021-02-21 18:53:48 -0500

    final submission

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,40 +1,32 @@
-let grid = document.getElementsByClassName("grid");
-let rowOne = document.getElementById("row-1");
-let rowTwo = document.getElementById("row-2");
-let rowThree = document.getElementById("row-3");
-let cellZ = document.getElementById("cell-0");
-let cellOne = document.getElementById("cell-1");
-let cellTwo = document.getElementById("cell-2");
-let cellThree = document.getElementById("cell-3");
-let cellFour = document.getElementById("cell-4");
-let cellFive = document.getElementById("cell-5");
-let cellSix = document.getElementById("cell-6");
-let cellSeven = document.getElementById("cell-7");
-let cellEight = document.getElementById("cell-8");
+//Starts Game Cells as Empty
+let board = ["", "", "", "", "", "", "", "", ""];
+
+//Sets Starting Player as Player X
+let currentPlayer = "X";
+
+//Player VS Player Start Button
 let start = document.getElementById("start");
-let statusText = document.getElementById("status-text");
-let cells = document.querySelectorAll(".cell");
-let playerNameInput = document.getElementById("player-form");
-let clock = document.getElementById("clock");
+
+//Player VS Computer Start Button
 let computer = document.getElementById("computer");
 
-//----need to develop a function for changing players-----//
+//Game Status Text
+let statusText = document.getElementById("status-text");
+
+//Player Names
+let playerNameInput = document.getElementById("player-form");
+
+//Game Timer
+let clock = document.getElementById("clock");
+
+//Empty Player Name Text
 let playerOne = "";
 let playerTwo = "";
 
+//Submit Form Starts as Deactivated
 submit.disabled = true;
 
-let count = 0;
-
-function timer() {
-  count++;
-  clock.textContent = `Time Elapsed: ${count} seconds`;
-}
-
-let startCount = (evt) => {
-  newCount = setInterval(timer, 1000);
-};
-
+//Event Listener placed to allow Players to Enter their Names
 playerNameInput.addEventListener("submit", (evt) => {
   evt.preventDefault();
   playerOne = document.getElementById("player-one");
@@ -46,10 +38,7 @@ playerNameInput.addEventListener("submit", (evt) => {
   }
 });
 
-let currentPlayer = "X";
-
-let board = ["", "", "", "", "", "", "", "", ""];
-
+//Stored Winning Cell Combinations
 let winCombo = [
   [0, 1, 2],
   [3, 4, 5],
@@ -61,6 +50,7 @@ let winCombo = [
   [2, 4, 6],
 ];
 
+//Checks Winning Combinations Against Player Cells
 function winCheck() {
   for (let i = 0; i <= 7; i++) {
     let winner = winCombo[i];
@@ -70,75 +60,109 @@ function winCheck() {
     if (winOne === "" || winTwo === "" || winThree === "") {
       continue;
     }
-    //------because current player alternates after each turn if it is X's turn and win condition has been met O is the winner-------
-    if (winOne === winTwo && winTwo === winThree && currentPlayer === "X") {
+
+    //Checks Winning Combinations Against Player O
+    if (winOne === winTwo && winTwo === winThree && winOne.includes("O")) {
       document.querySelectorAll(".cell").forEach((cell) => {
         if (winner.includes(parseInt(cell.id.split("-")[1]))) {
           cell.style.textDecoration = "line-through";
+          cell.style.backgroundColor = "red"
+          cell.style.color = "black"
         }
       });
+
+      //Inserts Player Name if Entered in Form
       if (playerOne.value !== undefined) {
         statusText.textContent = `${playerTwo.value} wins!`;
         start.disabled = false;
+        computer.disabled = false;
       } else {
         statusText.textContent = "Player O wins!";
         start.disabled = false;
+        computer.disabled = false;
       }
+
+      //Stops the clock from continuing to count after the game has been won
       clearInterval(newCount);
 
-      // setTimeout(() => {
-      // document.location = "/";
-      //  }, 3000);
+      //Refreshes Page after Game Over
+      setTimeout(() => {
+        document.location = "/";
+      }, 3000);
+
+      //Checks Winning Combinations Against Player X
     } else if (
       winOne === winTwo &&
       winTwo === winThree &&
-      currentPlayer === "O"
+      winOne.includes("X")
     ) {
+      //Crosses Out Winning Combination
       document.querySelectorAll(".cell").forEach((cell) => {
         if (winner.includes(parseInt(cell.id.split("-")[1]))) {
           cell.style.textDecoration = "line-through";
+          cell.style.backgroundColor = "red";
+          cell.style.color = "black";
         }
       });
+
+      //Inserts Player Name if Entered in Form
       if (playerOne.value !== undefined) {
         statusText.textContent = `Player ${playerOne.value} wins!`;
         start.disabled = false;
+        computer.disabled = false;
       } else {
         statusText.textContent = `Player X wins!`;
         start.disabled = false;
+        computer.disabled = false;
       }
+
+      //Stops the Clock from continuing to count after the game has been won
       clearInterval(newCount);
 
-      // setTimeout(() => {
-      //   document.location = "/";
-      // }, 3000);
+      //Refreshes Page after Game Over
+      setTimeout(() => {
+        document.location = "/";
+      }, 3000);
+
+      //Checks for a Draw
     } else if (
-      (!board.includes("") && winOne === "") ||
-      winTwo === "" ||
-      winThree === ""
+      !board.includes("") &&
+      !statusText.textContent.includes("wins!")
     ) {
       statusText.textContent = "Draw!";
+
+      //Stops the clock from continuing to count after the a draw has occurred
       clearInterval(newCount);
+
+      //Refreshes Page after Game Over
+      setTimeout(() => {
+        document.location = "/";
+      }, 3000);
     }
   }
 }
 
-
 //Player VS Player Game
-//---Moved start button eventListener and put the player vs player game inside it, going to change start button id to player vs player
+//Start Button Event Chain Reaction
 start.addEventListener("click", (evt) => {
   startCount();
   start.disabled = true;
   computer.disabled = true;
   submit.disabled = false;
-
   statusText.textContent = "Enter Player Names";
-  //----Used true and false values to trigger the players to change, no function needed, later changed values to X and O because they were put inside the board for win function
+
+  //User Input Cell Click Event Chain Reaction
   document.querySelectorAll(".cell").forEach((cell) => {
     cell.addEventListener("click", (evt) => {
+      
+      //Checks if Start Button was Pressed and If the Cell is Occupied
       if (start.disabled === true) {
         if (cell.textContent === "O" || cell.textContent === "X") {
           statusText.textContent = "Please select an empty cell.";
+
+          //Player X Input Translation
         } else if (currentPlayer === "X") {
+          cell.style.color = "red"
           board[cell.textContent] = currentPlayer;
           cell.textContent = "X";
           currentPlayer = "O";
@@ -147,7 +171,10 @@ start.addEventListener("click", (evt) => {
           } else {
             statusText.textContent = "Player O's Move";
           }
+
+          //Checks for Player O Turn and Input Translation
         } else {
+          cell.style.color = "red"
           board[cell.textContent] = currentPlayer;
           cell.textContent = "O";
           currentPlayer = "X";
@@ -157,6 +184,8 @@ start.addEventListener("click", (evt) => {
             statusText.textContent = "Player X's Move";
           }
         }
+        
+        //Checks if Game Over
         winCheck();
       }
     });
@@ -164,77 +193,73 @@ start.addEventListener("click", (evt) => {
 });
 
 //Computer VS Player Game
-
-let playerChoice
-
-
+//Start Button Event Chain Reaction
 computer.addEventListener("click", (evt) => {
   startCount();
   computer.disabled = true;
   start.disabled = true;
   statusText.textContent = "You are player X, good luck!";
 
+  //User Input Cell Click Event Chain Reaction
   document.querySelectorAll(".cell").forEach((cell) => {
     cell.addEventListener("click", (evt) => {
-      currentPlayer = "X";
+      
+      //Checks if Start Button was Pressed and If the Cell is Occupied
       if (computer.disabled === true) {
         if (cell.textContent === "O" || cell.textContent === "X") {
           statusText.textContent = "Please select an empty cell.";
+
+          //Updates to Generic Status and Translates User Click Input
         } else {
           statusText.textContent = "Human : X and Computer : O";
-          playerChoice = parseInt(cell.textContent)
+          cell.style.color = "red"
           board[cell.textContent] = currentPlayer;
           cell.textContent = "X";
+
+          //Checks if Game is Over
           winCheck();
-          currentPlayer = "O";
-          //----------------------------------------------------------------//
+
           //If Game Isn't Over the Computer Takes a Turn
           if (
-            statusText.textContent !== "Draw!" ||
-            statusText.textContent !== `Player X wins!` ||
-            statusText.textContent !== "Player O wins!"
+            !statusText.textContent.includes("wins!") &&
+            !statusText.textContent.includes("Draw!")
           ) {
-            
             //Computer Picks a Random Cell
             let randomMove = Math.floor(Math.random() * 9);
-            console.log(randomMove)
-            console.log(playerChoice)
-            
+
             //Random Cell is Available
-            if (board[randomMove] !== "X" && board[randomMove] !== "O" && randomMove !== playerChoice) {
+            if (board[randomMove] !== "X" && board[randomMove] !== "O") {
               board[randomMove] = "O";
-              console.log(board)
-              
+
               //Random Cell is Set to O and a Victory Check Occurs
               document.querySelectorAll(".cell").forEach((newCell) => {
-                if (parseInt(newCell.textContent) === randomMove && randomMove !== playerChoice) {
+                if (parseInt(newCell.textContent) === randomMove) {
+                  newCell.style.color = "red"
                   newCell.textContent = "O";
+
+                  //Checks if Game is Over
                   winCheck();
                 }
               });
-            
-              //Random Cell is Chosen Repeatedly Until an Available Cell is Matched
-            } else {
-              while (board[randomMove] === "X" && board[randomMove] === "O" && randomMove === playerChoice) {
-                randomMove = Math.floor(Math.random() * 9);
-                console.log(randomMove)
-                console.log(board)
-                console.log(playerChoice)
 
-                //Random Cell is Available
-                if (board[randomMove] !== "X" && board[randomMove] !== "O" && randomMove !== playerChoice) {
-                  board[randomMove] = "O";
-                  document.querySelectorAll(".cell").forEach((newCell) => {
-                    
-                    //Random Cell is Set to O and a Victory Check Occurs and Loop Ends
-                    if (parseInt(newCell.textContent) === randomMove && randomMove !== playerChoice) {
-                      newCell.textContent = "O";
-                      winCheck();
-                      return
-                    }
-                  });
+              //If Random Number Fails, First Available Cell is Set to O and a Victory Check Occurs
+            } else {
+              document.querySelectorAll(".cell").forEach((newerCell) => {
+                if (
+                  newerCell.textContent !== "X" &&
+                  newerCell.textContent !== "O"
+                ) {
+                  newerCell.style.color = "red"
+                  board[newerCell.textContent] = "O";
+                  newerCell.textContent = "O";
+
+                  //Checks is Game Over
+                  winCheck();
+
+                  //Hail Mary - Kicks Loop When Cell is Converted
+                  throw "This works! (and is intentional)";
                 }
-              }
+              });
             }
           }
         }
@@ -243,3 +268,61 @@ computer.addEventListener("click", (evt) => {
   });
 });
 
+//Time Keepers
+let hour = 0
+let minute = 0
+let seconds =  0 
+
+//Formatting Placeholders for Single Digits
+placeHolderS = 0
+placeHolderM = 0
+placeHolderH = 0
+
+//Timer Incrementing and Display Management
+function timer () {
+  seconds += 1
+ 
+  //Seconds Management
+  if (seconds > 59) {
+    minute += 1
+    seconds = 0
+  }
+  if (seconds > 9){
+    placeHolderS = ""
+  }
+  if (seconds < 10){
+    placeHolderS = 0
+  }
+
+  //Minute Management
+  if (minute > 59) {
+    hour += 1
+    minute = 0
+  }
+  if (minute > 9){
+    placeHolderM = ""
+  }
+  if (minute < 10){
+    placeHolderM = 0
+  }
+
+  //Hour Management
+  if (hour > 99) {
+    hour = 0
+  }
+  if (hour > 9){
+    placeHolderH = ""
+  }
+  if (hour < 10){
+    placeHolderH = 0
+  }
+
+  //Element Display Management
+  clock.textContent = ` Time Elapsed : ${placeHolderH}${hour}:${placeHolderM}${minute}:${placeHolderS}${seconds} `;
+
+}
+
+//Complete Timer Event
+let startCount = (evt) => {
+  newCount = setInterval(timer, 1000);
+};
